78c4c864d0aa163fb056f689656f6fe4bc0f725a
2023030106 <chenwei11@keshun.com.cn>
2024-12-17 18:08:07 +0800

    feature: 优化

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
-const cookieParser = require('cookie-parser');
 const app = express()
 
 const corsOptions = {
-  origin: 'http://172.16.11.110:5173',
+  origin: 'http://172.16.11.110:5173'
 }
 
-let kfzCookie = ''
-
 app.use(cors(corsOptions))
-app.use(cookieParser())
 
 // 有路搜索
 app.get('/youlu', async (req, res) => {
@@ -31,25 +27,39 @@ app.get('/youlu/detail', async (req, res) => {
 // 小谷吖搜索
 app.get('/xiaoguya', async (req, res) => {
 	const { isbn, token } = req.query
-	const { data: result } = await axios.get(`https://api.xiaoguya.com:9898/mall/api/mall/product/search/searchProduct?current=1&size=20&keyword=${isbn}`, {
-		headers: {
-			'Authorization': `bearer ${token}`,
-			'Content-Type': 'application/json'
+	try { 
+		const { data: result } = await axios.get(`https://api.xiaoguya.com:9898/mall/api/mall/product/search/searchProduct?current=1&size=20&keyword=${isbn}`, {
+			headers: {
+				'Authorization': `bearer ${token}`,
+				'Content-Type': 'application/json'
+			}
+		})
+		res.send(result)
+	} catch(err) {
+		if (err.response.status === 401) {
+			res.send({ code: 401, message: '小谷Token已过期，请更新Token'})
 		}
-	})
-	res.send(result)
+	}
 })
 
 // 小谷吖详情
 app.get('/xiaoguya/detail', async (req, res) => {
 	const { bookId, token } = req.query
-	const { data: result } = await axios.get(`https://api.xiaoguya.com:9898/mall/api/mall/product/infoById/${bookId}`, {
-		headers: {
-			'Authorization': `bearer ${token}`,
-			'Content-Type': 'application/json'
+	try {
+		const { data: result } = await axios.get(`https://api.xiaoguya.com:9898/mall/api/mall/product/infoById/${bookId}`, {
+			headers: {
+				'Authorization': `bearer ${token}`,
+				'Content-Type': 'application/json'
+			}
+		})
+		res.send(result)
+	} catch(err) {
+		if (err.response.status === 401) {
+			res.send({ code: 401, message: '小谷Token已过期，请更新Token'})
 		}
-	})
-	res.send(result)
+	}
+	
+	// res.send(result)
 })
 
 // 星辰搜索
@@ -69,13 +79,19 @@ app.get('/xc/detail', async (req, res) => {
 // 孔夫子搜索
 app.get('/kfz', async (req, res) => {
 	const { isbn, cookie } = req.query
-	console.log(cookie, '111111111')
 	const { data: result } = await axios.get(`https://search.kongfz.com/pc-gw/search-web/client/pc/product/keyword/list?dataType=0&keyword=${isbn}&page=1&size=50&sortType=7&actionPath=sortType&userArea=12001000000`, {
-		headers: {
-			cookie
-		}
+		headers: { cookie }
 	})
 	res.send(result)
+})
+
+// 孔夫子详情
+app.get('/kfz/detail', async (req, res) => {
+	const { shopId, itemId } = req.query
+	console.log(`https://book.kongfz.com/${shopId}/${itemId}/`)
+	const { data: html } = await axios.get(`https://book.kongfz.com/${shopId}/${itemId}/`)
+	res.setHeader('Content-Type', 'text/html')
+	res.send(html)
 })
 
 app.listen(3000, () => {
